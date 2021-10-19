8ec2633c032e22aee58b64a39715250702abd28d
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-19 22:22:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -108,37 +108,3 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         return
-
-@Bot.on_message(filters.command('filestore') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink)
-        ]
-    ]
-    try:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
-                )
-            ]
-        )
-    except IndexError:
-        pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
